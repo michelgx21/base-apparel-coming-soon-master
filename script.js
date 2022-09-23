25064b097c2f05ebe8ccf0cfb7e21a8291425e4d
michelgx21 <106442033+michelgx21@users.noreply.github.com>
2022-09-23 13:42:17 -0300

    Update script.js

--- script.js
+++ script.js
@@ -1,18 +1,21 @@
 const email=document.getElementById("email");
 const form=document.querySelector("form");
 const small=document.getElementById("text0");
-
+const i=document.querySelector("i");
 form.addEventListener("submit" ,(e)=>{
     if(email.value==""){
         text0.textContent="Email Address cannot be empty";
         email.style.borderColor="hsl(0, 93%, 68%)";
+        i.style.visibility="visible";
     }else if(checkEmail(email.value)!==true){
         text0.textContent="Please provide a valid email";
         email.style.borderColor="hsl(0, 93%, 68%)";
+        i.style.visibility="visible";
     }
     else{
         text1.textContent='';
         inputEmail.style.borderColor='';
+        i.style.visibility="hidden";
     }
 
     e.preventDefault()
